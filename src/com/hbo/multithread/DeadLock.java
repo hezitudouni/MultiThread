ea1688c5a66bbbbe90657195989e5f780bdaebc9
hezitudouni <49062335+hezitudouni@users.noreply.github.com>
2020-09-30 16:46:26 +0800

    [测试分支]

--- src/com/hbo/multithread/DeadLock.java
+++ src/com/hbo/multithread/DeadLock.java
@@ -10,6 +10,7 @@ package com.hbo.multithread;
 public class DeadLock {
 
     public static void  main(String[] args){
+        //测试将该版本提交到新的分支
         //Thread.sleep使当前线程放弃cpu的执行权以保证下一个线程可以获取到cpu的执行权，进入死锁状态
         //调试的工具有 1.jps搭配jstack 2.jvisualvm.exe 3.jconsole
         Object o1 = new Object();
