dfa9fbb42bda455437223f7f140fecefd0e5ff3e
hezitudouni <17611409527@163.com>
2020-12-07 23:36:30 +0800

    [多线程卖票实例添加注释,线程的六种状态]

--- src/com/hbo/multithread/SellTickets.java
+++ src/com/hbo/multithread/SellTickets.java
@@ -13,7 +13,7 @@ public class SellTickets { //资源类
     //三十张票
     private int total = 30;
 
-    //公共的买票方法
+    //公共的买票方法  操作
     //1.老的写法 synchronized
     public synchronized void sellTicket(){
         if(total>0){
@@ -36,8 +36,15 @@ public class SellTickets { //资源类
         }
 
     }
-
-
+    //      线程的运行状态   Thread.State枚举类
+    //         1.NEW  new Thread()
+    //         2.RUNNABLE 就绪 new Thread().satrt;
+    //         3.BLOCKED  阻塞 Thread.sleep() sleep不释放锁 ,object.wait() wait释放所;
+    //         4.WAITING  等待
+    //         5.TIMED_WAITING  带时间等候
+    //         6.TERMINATED 终止
+    //
+    //
     public static void main(String[] args) {
         //多线程操作  线程操作资源类
         SellTickets tickets = new SellTickets();
@@ -48,6 +55,8 @@ public class SellTickets { //资源类
         new Thread(()->{for(int i = 0;i<30;i++){tickets.sellTicket1();}},"E").start();
         new Thread(()->{for(int i = 0;i<30;i++){tickets.sellTicket1();}},"F").start();
         new Thread(()->{for(int i = 0;i<30;i++){tickets.sellTicket1();}},"G").start();
+
+
     }
 
 }
